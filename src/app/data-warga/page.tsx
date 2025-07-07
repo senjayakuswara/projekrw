e67fb53a019362bd83157f7cf569dae2a21fe503
Kuswara Senjaya <senjayakuswara13@gmail.com>
2025-07-07 12:42:53 +0000

    namun menurut saya untuk di hp statistik ini terlalu kebesaran.. sehingg

--- src/app/data-warga/page.tsx
+++ src/app/data-warga/page.tsx
@@ -460,7 +460,7 @@ export default function DataWargaPage() {
         </div>
       </div>
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+      <div className="grid grid-cols-2 gap-4 lg:grid-cols-4">
         <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">Total Keluarga</CardTitle>
