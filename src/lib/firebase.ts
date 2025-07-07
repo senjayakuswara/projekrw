bc882e4fd72828cea5e8087e5c3110378b9eb53e
Kuswara Senjaya <senjayakuswara13@gmail.com>
2025-07-07 13:25:36 +0000

    oke untuk sementara ini saja dulu.. sambil saya memikirkan fitur yang la

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTXWuD2uxYr2nSLC6IVLIHhgg_D18_boQ",
-  authDomain: "projek-rw.firebaseapp.com",
-  projectId: "projek-rw",
-  storageBucket: "projek-rw.appspot.com",
-  messagingSenderId: "880594762715",
-  appId: "1:880594762715:web:3a35bfc6057b565fea05fe"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
