bd7914ac7ef2b44ddc434bd4dbd1723feb5ed5eb
Kuswara Senjaya <senjayakuswara13@gmail.com>
2025-07-07 10:33:18 +0000

    ini: npm install firebase // Import the functions you need from the SDKs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDTXWuD2uxYr2nSLC6IVLIHhgg_D18_boQ",
+  authDomain: "projek-rw.firebaseapp.com",
+  projectId: "projek-rw",
+  storageBucket: "projek-rw.appspot.com",
+  messagingSenderId: "880594762715",
+  appId: "1:880594762715:web:3a35bfc6057b565fea05fe"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
